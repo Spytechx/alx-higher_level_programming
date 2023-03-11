b1c4d51fb4b6559a31d335a0a1a6aec214ba85f4
Spytechx <samuraidovexx@gmail.com>
2023-03-11 15:55:49 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
-
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
