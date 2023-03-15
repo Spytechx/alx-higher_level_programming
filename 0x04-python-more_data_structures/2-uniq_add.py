2ce383fdface4b74d64310863d9b95af84f9ac9b
Spytechx <samuraidovexx@gmail.com>
2023-03-16 00:57:26 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    add = 0
+    result = 0
     for i in set(my_list):
-        add += i
-    return add
+        result += i
+        return(result)
